f8e40080c9d93e73254120b84663b59f910df4bb
Job Chumo <43841454+cjkipu@users.noreply.github.com>
2020-04-21 15:20:07 +0300

    0.0.4

--- app/src/main/java/com/jobchumo/meme/Login.java
+++ app/src/main/java/com/jobchumo/meme/Login.java
@@ -4,20 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+    protected EditText emaili, passwo;
+    protected FirebaseAuth firebaseAuth;
+    protected ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
+        emaili = (EditText) findViewById(R.id.emailaddress);
+        passwo = (EditText) findViewById(R.id.passw);
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressBar = new ProgressBar(this);
     }
 
-    public void Log_in(View view){
+    public void Log_in(View view) {
+        String email = emaili.getText().toString().trim();
+        String pas = passwo.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pas)) {
+            Toast.makeText(Login.this, "Empty fields!! \n Please Enter all your details", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
+
     public void Sign_up(View view) {
         //finish();
         startActivity(new Intent(Login.this, SignUp.class));
--- app/src/main/java/com/jobchumo/meme/SignUp.java
+++ app/src/main/java/com/jobchumo/meme/SignUp.java
@@ -2,19 +2,57 @@ package com.jobchumo.meme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
+
+    protected EditText emailadd, pass, confpass;
+    protected ProgressBar progressBar;
+    protected FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_sign_up);
+
+        emailadd = (EditText) findViewById(R.id.email_address);
+        pass = (EditText) findViewById(R.id.password);
+        confpass = (EditText) findViewById(R.id.confirmpass);
+        progressBar = new ProgressBar(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null){
+
+        }
+
     }
     public void signup(View view){
+        String emaill = emailadd.getText().toString().trim();
+        String passwo = pass.getText().toString().trim();
+        String confirm = confpass.getText().toString().trim();
+
+        if (TextUtils.isEmpty(emaill) || TextUtils.isEmpty(passwo) || TextUtils.isEmpty(confirm)){
+            Toast.makeText(SignUp.this, "Empty fields!! \n Please Enter all your details", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (passwo.equals(confirm)){
+
+        }
+        else {
+            Toast.makeText(SignUp.this, "Passwords do not match!! \n Please try again", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
 
     }
 }
